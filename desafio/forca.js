1be35b5b13eba2b6b21669e0d4961ede7b80ea28
Pedro Marques <pedro.marques@ufpi.edu.br>
2022-07-23 21:55:25 -0300

    adiciona eslint e prettier

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,15 +1,16 @@
 class Forca {
+  chutar(letra) {}
 
-  chutar(letra) { }
-
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  buscarEstado() {
+    return "";
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+    return {
+      letrasChutadas: [], // Deve conter todas as letras chutadas
+      vidas: 6, // Quantidade de vidas restantes
+      palavra: [], // Deve ser um array com as letras que já foram acertadas ou o valor "_"
+    };
   }
 }
 
